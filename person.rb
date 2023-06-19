b7008b805b7742b8c90f93784449d023694edd6a
Booze33 <tebetisloh@gmail.com>
2023-06-19 17:02:05 -0500

    fixing linters

--- person.rb
+++ person.rb
@@ -1,34 +1,27 @@
 class Person
-  @id_counter = 0
-  attr_writer(:name, :age)
-  attr_reader(:id)
-
-  def initialize(name = 'Unknown', age = nil, parent_permission: true)
-    @id = generate_id
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
+    @id_counter = 0
+    attr_accessor :name, :age
+    attr_reader :id, :parent_permission
+  
+    def initialize(name = 'Unknown', age = nil, parent_permission: true)
+      @id = generate_id
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+    end
+  
+    def can_use_services?
+      of_age? || @parent_permission
+    end
+  
+    private
+  
+    def of_age?
+      @age.to_i >= 18
+    end
+  
+    def generate_id
+      @id_counter += 1
+    end
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def age=(age)
-    @age = age
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  private
-
-  def of_age?
-    @age.to_i >= 18
-  end
-
-  def generate_id
-    @id_counter += 1
-  end
-end
+  
