4f46b1c7c62fc439a02bfc68e2ff63ca8a7a6095
Booze33 <tebetisloh@gmail.com>
2023-06-20 18:56:43 -0500

    running test

--- decorate.rb
+++ decorate.rb
@@ -1,29 +1,13 @@
-require_relative 'person'
-
 class Nameable
   def correct_name
-    raise NotImplementedError 'Subclasses must implement correct_name method'
-  end
-end
-
-class Person < Nameable
-  attr_accessor :name, :age
-
-  def initialize(name = 'Unknown', age = nil)
-    super(correct_name, name, age)
-    @name = name
-    @age = age
-  end
-
-  def correct_name
-    name
+    raise NotImplementedError
   end
 end
 
 class Decorate < Nameable
   def initialize(nameable)
-    super()
     @nameable = nameable
+    super()
   end
 
   def correct_name
@@ -39,6 +23,7 @@ end
 
 class TrimmerDecorator < Decorate
   def correct_name
-    @nameable.correct_name[0...10] if @nameable.correct_name.length > 10
+    name = @nameable.correct_name
+    name.length > 10 ? name[0..9] : name
   end
 end
--- person.rb
+++ person.rb
@@ -1,33 +1,27 @@
-require_relative './decorate'
+require './decorate'
 
 class Person < Nameable
-  @id_counter = 0
-  attr_accessor :name, :age
-  attr_reader :id, :parent_permission
-
-  def initialize(name = 'Unknown', age = nil, parent_permission: true)
-    super(correct_name, name, age, parent_permission)
-    @id = generate_id
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
+  attr_accessor :name, :age
+  attr_reader :id
 
   def can_use_services?
     of_age? || @parent_permission
   end
 
-  private
-
-  def of_age?
-    @age.to_i >= 18
-  end
-
-  def generate_id
-    @id_counter += 1
-  end
-
   def correct_name
     @name
+  end
+
+  private
+
+  def of_age
+    @age >= 18
   end
 end
